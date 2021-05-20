dc3c9f94d13c1f3b1480330185db69c463fb50bd
Kevin Johnson <vadskye@gmail.com>
2021-05-20 09:52:54 -0700

    add generation for archetype rank abilities

--- src/classes/latex.rs
+++ src/classes/latex.rs
@@ -1,6 +1,6 @@
 mod basic_class_abilities;
 
-use crate::classes;
+use crate::classes::{self, archetype_rank_abilities, archetypes};
 use crate::latex_formatting;
 use titlecase::titlecase;
 
@@ -16,16 +16,28 @@ pub fn generate_latex_class_definition(class: &classes::Class) -> String {
 
             \\classbasics<Alignment> {class_alignment}.
 
-            \\classbasics<Archetypes> {class_name}s have the {archetypes} archetypes.
+            \\classbasics<Archetypes> {class_name}s have the {archetype_names} archetypes.
 
             {basic_class_abilities}
+
+            {archetypes}
         ",
-        archetype_table = generate_archetype_table(class),
-        // basic_class_abilities = basic_class_abilities::generate_latex_basic_class_abilities(class).trim(),
-        basic_class_abilities = "SNIP",
+        archetype_names = latex_formatting::join_string_list(&archetype_names).unwrap(),
+        archetype_table = generate_archetype_table(class).trim(),
+        archetypes = class
+            .archetypes()
+            .iter()
+            .map(
+                |a| generate_archetype_description(a, class.shorthand_name())
+                    .trim()
+                    .to_string()
+            )
+            .collect::<Vec<String>>()
+            .join("\n\n"),
+        basic_class_abilities =
+            basic_class_abilities::generate_latex_basic_class_abilities(class).trim(),
         class_name = titlecase(class.name()),
         class_alignment = class.alignment(),
-        archetypes = latex_formatting::join_string_list(&archetype_names).unwrap(),
     ));
 }
 
@@ -83,8 +95,8 @@ fn generate_ability_names_by_archetype_rank(class: &classes::Class) -> Vec<Strin
             if abilities_by_rank_and_archetype.get(rank).is_none() {
                 abilities_by_rank_and_archetype.push(Vec::new());
             }
-            abilities_by_rank_and_archetype[rank].push(latex_formatting::uppercase_first_letter(&
-                rank_abilities
+            abilities_by_rank_and_archetype[rank].push(latex_formatting::uppercase_first_letter(
+                &rank_abilities
                     .iter()
                     .map(|a| a.name.to_owned())
                     .collect::<Vec<String>>()
@@ -97,4 +109,44 @@ fn generate_ability_names_by_archetype_rank(class: &classes::Class) -> Vec<Strin
         abilities_by_rank.push(abilities_by_rank_and_archetype[rank].join(" & "));
     }
     return abilities_by_rank;
+}
+
+fn generate_archetype_description(
+    archetype: &archetypes::ClassArchetype,
+    class_shorthand: &str,
+) -> String {
+    return format!(
+        "
+            \\newpage
+            \\subsection<{archetype_name}>
+            {short_description}
+
+            {rank_abilities}
+        ",
+        archetype_name = titlecase(archetype.name()),
+        rank_abilities = archetype
+            .rank_abilities()
+            .iter()
+            .flatten()
+            .map(|a| generate_rank_ability(a, class_shorthand).trim().to_string())
+            .collect::<Vec<String>>()
+            .join("\n\n"),
+        short_description = archetype.short_description(),
+    );
+}
+
+fn generate_rank_ability(
+    rank_ability: &archetype_rank_abilities::RankAbility,
+    class_shorthand: &str,
+) -> String {
+    return format!(
+        "
+            \\cf<{class_shorthand}>[{rank}]<{ability_name}>
+            {ability_description}
+        ",
+        ability_description = rank_ability.description,
+        ability_name = titlecase(rank_ability.name),
+        class_shorthand = class_shorthand,
+        rank = rank_ability.rank,
+    );
 }
